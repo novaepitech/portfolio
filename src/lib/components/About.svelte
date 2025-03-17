d1d7303db7e1672aedeee4a2351bb1cd347ca91d
Mateo Cabrera <mateo.cabrera@epitech.eu>
2025-03-17 14:55:52 +0100

    refactor: translate About section to french

--- src/lib/components/About.svelte
+++ src/lib/components/About.svelte
@@ -1,10 +1,9 @@
 <script>
 	import { fly } from 'svelte/transition';
 	const aboutText = [
-		"Je suis un développeur Full Stack expérimenté de 22 ans avec un talent pour la création de produits qui font la différence. Mon expertise s'étend aux technologies frontend et backend, me permettant de concevoir des solutions complètes de A à Z.",
-		"Tout au long de ma carrière, j'ai été profondément impliqué dans l'écosystème des startups. J'ai travaillé avec diverses startups, adoptant le rythme rapide et l'esprit innovant de ces environnements. Ce parcours m'a conduit à fonder ma propre startup, où j'ai acquis une expérience directe dans tous les aspects du développement de produits et des opérations commerciales.",
-		"Pendant mes années de lycée, j'ai excellé en développement logiciel, obtenant le titre prestigieux de DUX (meilleur élève) dans ce domaine. Cette réussite précoce a jeté les bases de ma passion et de mon expertise en programmation.",
-		"Lorsque je ne suis pas absorbé par le code, vous me trouverez en train d'expérimenter dans la cuisine ou à donner vie à des idées avec mon imprimante 3D. Je suis toujours en train de créer, qu'il s'agisse de produits numériques ou d'objets tangibles."
+		"Attiré par l'informatique depuis mon plus jeune âge, j'ai toujours été fasciné par le fonctionnement interne des systèmes. Déjà à 14 ans, je rootais et jailbreakais mes téléphones, explorant les possibilités au-delà des limites imposées.",
+		"Aujourd'hui âgé de 19 ans, je suis étudiant en deuxième année à Epitech.  Je me spécialise dans le développement web fullstack, un domaine qui me passionne car j'aime créer des applications offrant une expérience utilisateur intuitive et agréable. Pour moi, une application réussie doit être à la fois belle et performante.",
+		"Quand je ne suis pas en train de coder, vous me trouverez en train d'explorer un nouveau monde en VR, ou bien en train de faire des pompes et des tractions."
 	];
 </script>
 
